7294c351b1d62aaf995e47b8755f4628bbf0db76
sofia cronström <sofiacronstrom@sofias-MacBook-Pro.local>
2022-01-24 16:26:53 +0100

    adding point and directional light and an universal camera

--- src/scenes/loadModelAndEnv.ts
+++ src/scenes/loadModelAndEnv.ts
@@ -1,17 +1,21 @@
-import { Engine } from "@babylonjs/core/Engines/engine";
-import { Scene } from "@babylonjs/core/scene";
-import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
-import { Vector3 } from "@babylonjs/core/Maths/math.vector";
-import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { Engine, 
+    Scene, 
+    ArcRotateCamera,
+    Vector3,
+    HemisphericLight, 
+    SceneLoader, 
+    FreeCamera, 
+    StandardMaterial, 
+    UniversalCamera,
+    DirectionalLight,
+    PointLight
+ } from "@babylonjs/core";
+
 import { CreateSceneClass } from "../createScene";
-import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
-import { CubeTexture } from "@babylonjs/core/Materials/Textures/cubeTexture";
-import { EnvironmentHelper } from "@babylonjs/core/Helpers/environmentHelper";
 
 // required imports
 import "@babylonjs/core/Loading/loadingScreen";
 import "@babylonjs/loaders/glTF";
-import "@babylonjs/core/Materials/standardMaterial";
 import "@babylonjs/core/Materials/Textures/Loaders/envTextureLoader";
 
 
@@ -28,14 +32,16 @@ export class LoadModelAndEnvScene implements CreateSceneClass {
         const scene = new Scene(engine);
 
         // This creates and positions a free camera (non-mesh)
-        const camera = new ArcRotateCamera(
-            "my first camera",
-            0,
-            Math.PI / 3,
-            10,
-            new Vector3(0, 0, 0),
-            scene
-        );
+            const camera = new ArcRotateCamera(
+                "my first camera",
+                0,
+                Math.PI / 3,
+                10,
+                new Vector3(0, 0, 0),
+                scene
+            );
+
+            // var camera = new UniversalCamera("UniversalCamera", new Vector3(0, 2, -10), scene);
 
         // This targets the camera to scene origin
         camera.setTarget(Vector3.Zero());
@@ -43,19 +49,19 @@ export class LoadModelAndEnvScene implements CreateSceneClass {
         // This attaches the camera to the canvas
         camera.attachControl(canvas, true);
 
-        camera.useFramingBehavior = true;
+        //camera.useFramingBehavior = true;
 
        
 
         // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-        const light = new HemisphericLight(
-            "light",
-            new Vector3(0, 1, 0),
-            scene
-        );
+        const light1 = new PointLight("spot02", new Vector3(476.92, 1167.96, 1329.6),
+        scene);
+        const light2: DirectionalLight = new DirectionalLight("light3", new Vector3(-1,-2,-1), scene)
+        light2.direction = new Vector3(0.72, -0.21, -0.67);
+        light2.intensity = 0.7
 
-        // Default intensity is 1. Let's dim the light a small amount
-        light.intensity = 0.7;
+        
+        
 
         const importResult = await SceneLoader.ImportMeshAsync(
             "",
