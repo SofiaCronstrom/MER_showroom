6a23085f254680e5854128481fd6fc03a81b8b97
sofia cronström <sofiacronstrom@sofias-MacBook-Pro.local>
2022-02-21 17:35:19 +0100

    changing visibility on collision meshes

--- src/scenes/collision.ts
+++ src/scenes/collision.ts
@@ -14,7 +14,7 @@ import {
      const collPlane: Mesh = MeshBuilder.CreatePlane('collisionplane',{width: 1550, height: 600});
      collPlane.position = new Vector3(0, 254.98, -1032.14);
      collPlane.rotation = new Vector3(0, Math.PI/1, 0)
-     //collPlane.isVisible = false;
+     collPlane.isVisible = false;
      collPlane.showBoundingBox = true;
 
      const collPlane2 = collPlane.clone('collisionplane2');
@@ -25,7 +25,7 @@ import {
      collPlaneLong.position = new Vector3(-743.708, 254.98, 218.37);
      collPlaneLong.rotation = new Vector3(0, -Math.PI/2, 0);
      collPlaneLong.showBoundingBox = true;
-     //collPlaneLong.isVisible = false;
+     collPlaneLong.isVisible = false;
      const collPlaneLong2 = collPlaneLong.clone('collisionLong2');
      collPlaneLong2.position = new Vector3(743.708, 254.98, 218.37);
      collPlaneLong2.rotation = new Vector3(0, Math.PI/2, 0);
@@ -35,7 +35,7 @@ import {
      collStairs.position = new Vector3(-293.46, 100.91, -340.42);
      collStairs.rotation = new Vector3((51.11*Math.PI)/180, -Math.PI/1, -Math.PI/2)
      collStairs.showBoundingBox = true;
-     //collStairs.isVisible = false;
+     collStairs.isVisible = false;
 
     let collArr = [collPlane, collPlane2, collPlaneLong, collPlaneLong2]
 
