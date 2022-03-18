509ba1c412e4bb47d4833cf56090083106ae1315
sofia cronström <sofiacronstrom@sofias-MacBook-Pro.local>
2022-03-18 17:02:36 +0100

    adding pause and play logic on video

--- src/scenes/video.ts
+++ src/scenes/video.ts
@@ -9,16 +9,20 @@ import {
     StandardMaterial,
     VideoTexture
  } from "@babylonjs/core";
+import { PointerEventTypes } from "babylonjs";
 
 import video from '../../assets/video.mp4';
 
+
+
  export const Video = (scene: Scene) => {
 
-
+    //VIDEO CONTAINER
     const videoPlane: Mesh = MeshBuilder.CreatePlane('videoplane', {width: 150, height: 100, sideOrientation: Mesh.DOUBLESIDE}, scene);
     videoPlane.position = new Vector3(119, 127,1177)
     videoPlane.rotation = new Vector3(0,61,0)
-
+   
+   //MP4
    const videoTextMat = new StandardMaterial('materialVideo', scene);
    const videoTexture = new VideoTexture('videotexture', video, scene);
    
@@ -28,5 +32,19 @@ import video from '../../assets/video.mp4';
    videoPlane.material = videoTextMat;
 
 
-return scene;
+   //LOGIC FOR VIDEO
+   scene.onPointerObservable.add( (evt: any) => {
+    if(evt.pickInfo.pickedMesh === videoPlane){
+        if(videoTexture.video.paused)
+        videoTexture.video.play();
+    else
+        videoTexture.video.pause();
+    console.log(videoTexture.video.paused?"paused":"playing"); 
+    }
+   }, PointerEventTypes.POINTERPICK);
+
+   
+
+
+  return scene;
  }
